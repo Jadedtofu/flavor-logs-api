ab4818ff38a3283cd2c0da3ee4d8a9df22aff3d7
Xiaolei Wang <xwang@sandiego.edu>
2019-12-28 20:16:19 -0800

    created migrations and seed files

---
+++ seeds/seed.flavolorlogs_eateries.sql
@@ -0,0 +1,5 @@
+INSERT INTO flavorlogs_eateries (name, phone, address, notes)
+VALUES
+    ('Pho Mignon','760-320-5210', '1235 North Island Street, San Diego, CA 92108',  'Open M-F 9 am - 9 pm, Sat 9 am - 11 pm, Sun 10 am - 8 pm'),
+    ('200° Bakery', '619-280-7599', '8590 Elm Wake Drive, San Diego, CA 92123', 'Open M-F 7 am - 5 pm, Sat 7 am - 8 pm, Sun 9 am - 3 pm'),
+    ('Mana Noodlehouse', '858-780-2323', '590 Convoy Street, San Diego, CA 92117', 'Open M-F 11 am - 9 pm, Sat 11 am - 12 am, Sun 11 am - 6 pm');
