635203ed2b036f56b56c32279da24842aeb0cdbd
Xiaolei Wang <xwang@sandiego.edu>
2019-12-28 17:53:23 -0800

    set up migrations and migration scripts for tables in database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    // DB_URL: 
-    // TEST_DB_URL: 
+    DB_URL: process.env.DB_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs",
+    TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs-test"
 }
