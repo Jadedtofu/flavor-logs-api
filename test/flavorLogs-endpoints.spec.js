c8f5c497be4cd087fbc335481a7ed36bf9e2ae81
Xiaolei Wang <xwang@sandiego.edu>
2019-12-29 18:25:08 -0800

    tetsted endpoings for flavor logs post

--- test/flavorLogs-endpoints.spec.js
+++ test/flavorLogs-endpoints.spec.js
@@ -55,4 +55,53 @@ describe('FlavorLogs Endpoints', () => {
         });
     });
 
+    describe(`POST /api/flavorLogs`, () => {
+        const testEateries = makeEateriesArray();
+        const testFlavorLogs = makeFlavorLogsNoId();
+
+        beforeEach(`insert Flavor Logs`, () => {
+            return db
+                .into(`flavorlogs_eateries`)
+                .insert(testEateries)
+                .then(() => {
+                    return db
+                        .into(`flavorlogs_logs`)
+                        .insert(testFlavorLogs);
+                });
+        });
+
+        it(`creates a flavor log, responds with 201 and the new log`, () => {
+            const newFlavorLog = {
+                title: 'New Title',
+                info: 'New Flavor Log Info',
+                ordered: 'New Order',
+                rating: 5,
+                date: "2019-12-29",
+                image_link: "",
+                eatery_id: 2
+            }
+
+            return supertest(app)
+                .post(`/api/flavorLogs`)
+                .send(newFlavorLog)
+                .expect(201)
+                .expect(res => {
+                    expect(res.body.title).to.eql(newFlavorLog.title)
+                    expect(res.body.info).to.eql(newFlavorLog.info)
+                    expect(res.body.ordered).to.eql(newFlavorLog.ordered)
+                    expect(res.body.rating).to.eql(newFlavorLog.rating)
+                    expect(res.body.date).to.eql(newFlavorLog.date)
+                    expect(res.body.image_link).to.eql(newFlavorLog.image_link)
+                    expect(res.body).to.have.property('id')
+                    expect(res.body).to.have.property('eatery_id')
+                    expect(res.headers.location).to.eql(`/api/flavorLogs/${res.body.id}`)
+                })
+                .then(res => {
+                    supertest(app)
+                        .get(`/api/flavorLogs/${res.body.id}`)
+                        .expect(res.body);
+                });
+        });
+    });
+
 });
