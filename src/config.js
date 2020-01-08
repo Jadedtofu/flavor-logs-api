faba70787ba72be39b4fb02433c24355187fad6d
Xiaolei Wang <xwang@sandiego.edu>
2020-01-07 22:16:17 -0800

    preparing api for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs",
-    TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs-test"
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs-test",
+    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 
 }
