40c11eb437ae29e948eeca30b1ca5d9521eb2ad6
Xiaolei Wang <xwang@sandiego.edu>
2020-01-07 22:57:18 -0800

    updated configs for deployment

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://flavorlogs_user:flavor@localhost/flavorlogs-test",
-    // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://hidden-reaches-65796.herokuapp.com/api"
 }
